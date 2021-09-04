3ac87f0403efb2bcbce82a5b81a5d92cdfce9edf
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:23:45 +0200

    Projektdateien hinzufügen.

---
+++ Switch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int monat = 0;
+
+            switch(monat)
+            {
+                case 1:
+                    Console.WriteLine("Januar");
+                break;
+
+                case 2:
+                    Console.WriteLine("Februar");
+                break;
+
+                case 3:
+                    Console.WriteLine("März");
+                break;
+
+                default:
+                    Console.WriteLine("Fehlrhafter Monat");
+                break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
